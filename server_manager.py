da9eccfc452fbb51a32724bf16b17df7961383f8
wrongwayboyyyy <wrongwayboyy@gmail.com>
2020-09-03 01:00:23 +0400

    VkResendBot for humans

--- server_manager.py
+++ server_manager.py
@@ -1,5 +1,13 @@
 from server import Server
 from config import vk_api_token, group_id
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 server1 = Server(vk_api_token, group_id, "server1")
 server1.start()
